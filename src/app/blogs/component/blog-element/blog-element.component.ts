026be6287dd74e0fd9dee2204537be09ee4e8bc7
boreq <boreq@0x46.net>
2018-10-18 00:41:28 +0200

    Add call to action

--- src/app/blogs/component/blog-element/blog-element.component.ts
+++ src/app/blogs/component/blog-element/blog-element.component.ts
@@ -1,17 +1,19 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Blog} from '../../dto/blog';
+import {CleanupSubscriptionsComponent} from '../../../base/component/cleanup-subscriptions/cleanup-subscriptions.component';
+import {AuthEventsService} from '../../../auth/service/auth-events.service';
+import {User} from '../../../auth/dto/user';
+import {AuthModalService} from '../../../auth/service/auth-modal.service';
 
 @Component({
     selector: 'app-blog-element',
     templateUrl: './blog-element.component.html',
     styleUrls: ['./blog-element.component.scss']
 })
-export class BlogElementComponent {
+export class BlogElementComponent extends CleanupSubscriptionsComponent implements OnInit {
 
     @Input()
     blog: Blog;
-
-    cleanUrl: string;
 
     @Output()
     subscribe = new EventEmitter<Blog>();
@@ -19,12 +21,33 @@ export class BlogElementComponent {
     @Output()
     unsubscribe = new EventEmitter<Blog>();
 
+    user: User;
+
+    constructor(private authEventsService: AuthEventsService,
+                private authModalService: AuthModalService) {
+        super();
+    }
+
+
+    ngOnInit(): void {
+        this.registerSubscription(
+            this.authEventsService.loggedIn$
+                .subscribe(user => {
+                    this.user = user;
+                })
+        );
+    }
+
     onSubscribeClick(event: any): void {
         this.subscribe.next(this.blog);
     }
 
     onUnsubscribeClick(event: any): void {
         this.unsubscribe.next(this.blog);
+    }
+
+    showRegisterPopup(): void {
+        this.authModalService.openRegisterModal();
     }
 
 }
